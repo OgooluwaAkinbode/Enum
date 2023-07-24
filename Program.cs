a20887c2b72719a51ea37094503613609e0f9239
OgooluwaAkinbode <Ogooluwaakinbode@gmail.com>
2023-07-24 14:38:52 +0100

    lak

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using Internal;
+using System.Reflection.Emit;
+using System;
+namespace Enum
+{
+	
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			// Months mon = Months.April;
+			// Console.WriteLine(mon);
+			
+			// string month= Months.April.ToString();
+			// Console.WriteLine(month);
+			
+			// int num=(int)Months.August;
+			// Console.WriteLine(num);
+			
+			// string days= Days.Monday.ToString();
+			Console.WriteLine(days);
+	
+			
+		}
+	}
+}
+
